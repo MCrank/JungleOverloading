12de1b9f733e14eacf7ae2078494376f3b2f1d92
MCrank <marco_mcse@hotmail.com>
2019-03-08 03:54:35 -0600

    Jungle Overloading exercise

--- JungleOverloading/Program.cs
+++ JungleOverloading/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using JungleOverloading.Employees;
+using System;
+using System.Collections.Generic;
 
 namespace JungleOverloading
 {
     class Program
     {
+        public static List<Employee> Employees { get; private set; } = new List<Employee>();
+
+        //public List<Employee> Employees { get; set; } = new List<Employee>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee marco = new Employee("Marco", "Crank");
+            Employee shane = new Employee("Shane", "Wilson");
+            Employee maggie = new Employee("Maggie", "Leavell");
+
+            Employees.Add(marco);
+            Employees.Add(shane);
+            Employees.Add(maggie);
+
+            Console.WriteLine("\n*** Overload #1 ****");
+            marco.Eat();
+
+            Console.WriteLine("\n*** Overload #2 ****");
+            marco.Eat("Pizza");
+
+            Console.WriteLine("\n*** Overload #3 ****");
+            marco.Eat(Employees);
+
+            Console.WriteLine("\n*** Overload #4 ****");
+            marco.Eat("Tacos", Employees);
+
+            Console.ReadLine();
+
         }
     }
 }
